428032f7e60ae2f19e20fb1a4888edb4b9ae2668
aryalsohan0 <aryalsohan0@gmail.com>
2020-12-21 14:01:51 +0545

    Upto tutorial 3

---
+++ tut3_morePrinting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "boy i love bacon" << endl; //endl will end the line and starts new line
+    cout << "and bacon\n"; //\n is used for next line with escape character as backslash
+    cout << "This will appear in new line";
+
+    return 0;
+}
